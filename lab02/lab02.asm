08f10339f09058a0367d61f4482dbf0642b93b58
antigoniemmanouil <antigoni.e.m@windowslive.com>
2017-03-08 20:54:04 +0200

    Update lab02.asm

--- lab02/lab02.asm
+++ lab02/lab02.asm
@@ -1,6 +1,6 @@
 # Find longest word in a string
 #   in MIPS assembly using MARS
-# for MYΥ-402 - Computer Architecture
+#Β for MYΞ¥-402 - Computer Architecture
 # Department of Computer Engineering, University of Ioannina
 # Aris Efthymiou
 
@@ -12,18 +12,28 @@ main:
         la         $a0, string         # The address of the string
         addu       $v1, $zero, $a0     # The address of longest word
 
-        ########################################################################
-        #  Write your code here
-        # NOTE: Don't print out the results! Automatic testing will get the final
-        #  value of $v1 and check if it is correct
-        ########################################################################
+        addu 		$w1, $zero, $zero
+	addiu      $w0, $zero, 32
+	addu 	$s1, $zero, $zero
+	
+compare:
+	slt 		$w2, $w1, $s1  
+	beq 		$w2, $zero, max
+	addiu 	$a0, $a0, 1
+	addu	 	$w1, $zero, $zero
+	j loop	
+	
+max: 	              
+	subu	 	$v1, $a0, $w1  
+	addu		$s1, $zero, $w1 
+	addu	 	$w1, $zero, $zero 
+	addiu 	$a0, $a0, 1
+	j loop
         
 exit: 
         addiu      $v0, $zero, 10    # system service 10 is exit
         syscall                      # we are outta here.
         
-        ###############################################################################
-        # Data input.
-        ###############################################################################
+       
         .data
 string: .asciiz "small  equal long atTheEnd"
